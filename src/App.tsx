1c87f7f13b52cbe47f482754612bc4ce612039a2
chienli <chienli@chienlideMacBook-Pro.local>
2022-01-24 23:15:29 +0800

    feat: 新增路由以及基础文件夹配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router,Route,Redirect,Switch } from 'react-router-dom'
+import './App.scss'
+import Layout from './pages/Layout'
+import Login from '@/pages/Login'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="app">
+    <Router>
+      <Switch>
+        <Redirect exact from='/' to='/home'></Redirect>
+        <Route path={'/home'} component={Layout}></Route>
+        <Route path={'/login'} component={Login}></Route>
+      </Switch>
+    </Router>
+  </div>
 }
 
-export default App;
+export default App
---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,7 @@
+import { Button } from 'antd-mobile'
+export default function Login() {
+  return <div>
+      LOGIN
+      <Button color="primary">Button</Button>
+  </div>;
+}
